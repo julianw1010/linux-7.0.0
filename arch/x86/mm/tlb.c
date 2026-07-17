d7c041e311d366f60d4988bc78472af7a1d695d7
Julian Waciewski <julian.waciewski@rwth-aachen.de>
2026-07-17 12:24:58 +0200

    ptcache: restrict invlpgb knob to cache-only processes

--- arch/x86/mm/tlb.c
+++ arch/x86/mm/tlb.c
@@ -449,7 +449,8 @@ static void consider_global_asid(struct mm_struct *mm)
 	if (!cpu_feature_enabled(X86_FEATURE_INVLPGB))
 		return;
 
-	if (!READ_ONCE(sysctl_ptcache_invlpgb))
+	if (!READ_ONCE(sysctl_ptcache_invlpgb) &&
+	    READ_ONCE(mm->cache_only_mode))
 		return;
 
 	/* Check every once in a while. */
